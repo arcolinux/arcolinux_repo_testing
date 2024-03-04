c263ef484f666ff7346868c92ce5e9458de33750
Erik Dubois <erik.dubois@gmail.com>
2024-03-04 16:39:02 +0100

    update

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -7,18 +7,6 @@ repo-add -s -n -R arcolinux_repo_testing.db.tar.gz *.pkg.tar.zst
 
 sleep 1
 
-rm arcolinux_repo_testing.db
-rm arcolinux_repo_testing.db.sig
-
-rm arcolinux_repo_testing.files
-rm arcolinux_repo_testing.files.sig
-
-mv arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
-mv arcolinux_repo_testing.db.tar.gz.sig arcolinux_repo_testing.db.sig
-
-mv arcolinux_repo_testing.files.tar.gz arcolinux_repo_testing.files
-mv arcolinux_repo_testing.files.tar.gz.sig arcolinux_repo_testing.files.sig
-
 echo "####################################"
 echo "Repo Updated!!"
 echo "####################################"
